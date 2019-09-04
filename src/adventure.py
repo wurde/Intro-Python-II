e94fa57771947fe33a974189d6f0b2ede6722430
Andy Bettisworth <andy@accreu.com>
2019-09-04 17:27:24 -0500

    compose game as method

--- src/adventure.py
+++ src/adventure.py
@@ -12,20 +12,20 @@ from player import Player
 #
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+  'outside':  Room("Outside Cave Entrance",
+                   "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+  'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+  'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+  'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+  'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -44,16 +44,13 @@ room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
 #
-# Initialize player
+# Define game
 #
 
-main_player = Player('Joe', 'overlook')
+def adventure():
+  main_player = Player('Joe', 'overlook')
 
-#
-# Main loop
-#
-
-while True:
+  while True:
     print('You wake up.\n')
     print(f"// {main_player.current_room.upper()}\n")
 
@@ -66,5 +63,12 @@ while True:
       print('// GAME OVER\n')
       sys.exit(0)
 
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+
+#
+# Initialize game
+#
+
+if __name__ == '__main__':
+  adventure()
